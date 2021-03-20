66f7c0d767ab34e3fbc45eaec6a25575c4404105
Jeff Guebert <jeff@jeffguebert.com>
2021-03-19 21:55:41 -0700

    Added download route

--- ombro.py
+++ ombro.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    subprocess.Popen(['./bin/youtube-dl.exe', request.form['download_url'], '-o', './output/%(title)s.%(ext)s'])
+    return redirect('/?download_started=1')
