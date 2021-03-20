897d78c14d88dd11124a372813f6c190c4d8ca69
Jeff Guebert <jeff@jeffguebert.com>
2021-03-19 22:39:21 -0700

    Name of downloaded video shown

--- ombro.py
+++ ombro.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, redirect, request
 import subprocess
+import urllib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    context = {}
+    context['download_name'] = request.args.get('download_started', type=str)
+    return render_template('index.html', context=context)
 
 @app.route('/download', methods=['POST'])
 def download():
-    subprocess.Popen(['./bin/youtube-dl.exe', request.form['download_url'], '-o', './output/%(title)s.%(ext)s'])
-    return redirect('/?download_started=1')
+    download_name = subprocess.run(['./bin/youtube-dl.exe', request.form['download_url'], '-e'], capture_output=True).stdout
+    download = subprocess.Popen(['./bin/youtube-dl.exe', request.form['download_url'], '-o', './output/%(title)s.%(ext)s'])
+    redirect_path = '/?download_started=' + str(urllib.parse.quote(download_name))
+    return redirect(redirect_path)
